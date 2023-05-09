6084d04d179d2dd9e9eca26c747e9eab7f603fb1
badalvashishtha <badalvashishtha2004@gmail.com>
2023-05-09 16:36:45 +0530

    Remove Fractional

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,7 +1,6 @@
 import view from './view.js';
 
 import icons from 'url:../../img/icons.svg';
-import { Fraction } from 'fractional';
 
 class RecipeView extends view {
   _parentElement = document.querySelector('.recipe');
@@ -128,9 +127,7 @@ class RecipeView extends view {
       <svg class="recipe__icon">
        <use href="${icons}#icon-check"></use>
       </svg>
-      <div class="recipe__quantity">${
-        ing.quantity ? new Fraction(ing.quantity).toString() : ''
-      }</div>
+      <div class="recipe__quantity">${ing.quantity ? ing.quantity : ''}</div>
       <div class="recipe__description">
        <span class="recipe__unit">${ing.unit}</span>
        ${ing.description}
